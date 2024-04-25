b53d31f09f615195c43d5951bbefbc779e21294d
Roman Chursanov <authfailed@gmail.com>
2024-04-26 04:03:51 +0500

    added code for url shortener

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,14 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("form");
-  form.addEventListener("submit", handleFormSubmit);
+  const form_mac = document.getElementById("form-mac");
+  const form_link = document.getElementById("form-link");
+  form_mac.addEventListener("submit", handleFormSubmitMac);
+  form_link.addEventListener("submit", handleFormSubmitLink);
 });
 
-async function handleFormSubmit(event) {
+async function handleFormSubmitMac(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const inputField = formData.get("inputField");
+  const inputField = formData.get("input-mac");
 
   try {
     const response = await fetch(
@@ -26,15 +28,40 @@ async function handleFormSubmit(event) {
     }
 
     const result = await response.json();
-    console.log(result);
 
     // Assuming the API returns an array of objects and you want the company name from the first object
     const companyName = result[0]?.company;
 
     if (companyName) {
-      document.getElementById("inputField").value = companyName;
+      document.getElementById("input-mac").value = companyName;
     } else {
-      console.error("Company name not found in the response");
+      document.getElementById("input-mac").value = "Не найдено";
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}
+
+async function handleFormSubmitLink(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const inputField = formData.get("input-link");
+
+  try {
+    const response = await fetch(`https://clck.ru/--?url=${inputField}`, {
+      method: "GET",
+    });
+    if (!response.ok) {
+      console.error("Network response was not ok", response.status);
+      return;
+    }
+
+    const result = await response.text();
+
+    if (result) {
+      await navigator.clipboard.writeText(result);
+      $.notify("Скопировано", "success");
+      document.getElementById("input-link").value = result;
     }
   } catch (error) {
     console.error("Fetch error:", error);
