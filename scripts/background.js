8226943690e8233655ea4c86439b1546f2e72e10
AuthFailed <authfailed@gmail.com>
2024-05-13 16:05:04 +0500

    fixed copypaste

--- scripts/background.js
+++ scripts/background.js
@@ -1,19 +1,18 @@
 ﻿setTimeout(function () {
   document.getElementById("waiter").parentNode.removeAttribute("style");
   document.getElementById("waiter").parentNode.removeAttribute("onselectstart");
+  document.getElementById("formform").onmousedown = function (event) {
+    event.stopPropagation();
+  };
+  document.getElementById("formform").onselectstart = function (event) {
+    event.stopPropagation();
+  };
 
   document.getElementById("waiter").onselectstart = function (event) {
     event.stopPropagation();
   };
 
   document.getElementById("waiter").onmousedown = function (event) {
-    event.stopPropagation();
-  };
-
-  document.getElementById("formform").onmousedown = function (event) {
-    event.stopPropagation();
-  };
-  document.getElementById("formform").onselectstart = function (event) {
     event.stopPropagation();
   };
 }, 2000);
