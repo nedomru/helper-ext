7f4b18c166f694ebd9efc0538e29ed9d87475567
AuthFailed <authfailed@gmail.com>
2024-05-28 18:31:58 +0500

    Кнопки в гену

--- scripts/genesysEssentials.js
+++ scripts/genesysEssentials.js
@@ -24,20 +24,51 @@ function hideGenesysButtons() {
 }
 
 function genesysButtons() {
+  // ЧатМастер
+  const chatMaster = document.createElement("a");
+  chatMaster.href = "http://cm.roool.ru/";
+  chatMaster.target = "_blank";
+  chatMaster.textContent = "ЧМ";
+  chatMaster.classList.add("ml-2", "d-inline-block", "align-middle");
+
+  // Настройка ТВ, стили
+  chatMaster.style.borderRight = "1px solid white";
+  chatMaster.style.fontSize = "1rem";
+  chatMaster.style.color = "white";
+  chatMaster.style.lineHeight = "40px";
+  chatMaster.style.marginLeft = "20px";
+  chatMaster.style.marginRight = "10px";
+  chatMaster.style.paddingRight = "10px";
+  chatMaster.style.cursor = "pointer";
+  chatMaster.addEventListener("mouseenter", () => {
+    chatMaster.style.color = "#ccc";
+  });
+  chatMaster.addEventListener("mouseleave", () => {
+    chatMaster.style.color = "white";
+  });
+
+  // ------------
+
   // Настройка роутеров
   const setupRouter = document.createElement("a");
   setupRouter.href =
     "https://interzet.dom.ru/service/knowledgebase/internet/kak-nastroit-router";
   setupRouter.target = "_blank";
-  setupRouter.textContent = "Настройка роутеров";
+  setupRouter.textContent = "Роутеры";
 
   // Настройка роутеров, стили
   setupRouter.classList.add("ml-2", "d-inline-block", "align-middle");
   setupRouter.style.fontSize = "1rem";
   setupRouter.style.color = "white";
-  setupRouter.style.lineHeight = "40px";
   setupRouter.style.marginRight = "10px";
   setupRouter.style.paddingRight = "10px";
+  setupRouter.style.cursor = "pointer";
+  setupRouter.addEventListener("mouseenter", () => {
+    setupRouter.style.color = "#ccc";
+  });
+  setupRouter.addEventListener("mouseleave", () => {
+    setupRouter.style.color = "white";
+  });
 
   // ------------
 
@@ -46,7 +77,7 @@ function genesysButtons() {
   setupTV.href =
     "https://oren.dom.ru/faq/televidenie/kak-nastroit-cifrovye-kanaly-na-televizore";
   setupTV.target = "_blank";
-  setupTV.textContent = "Настройка ТВ";
+  setupTV.textContent = "Телевизоры";
   setupTV.classList.add("ml-2", "d-inline-block", "align-middle");
 
   // Настройка ТВ, стили
@@ -54,26 +85,45 @@ function genesysButtons() {
   setupTV.style.color = "white";
   setupTV.style.marginRight = "10px";
   setupTV.style.paddingRight = "10px";
+  setupTV.style.cursor = "pointer";
+  setupTV.addEventListener("mouseenter", () => {
+    setupTV.style.color = "#ccc";
+  });
+  setupTV.addEventListener("mouseleave", () => {
+    setupTV.style.color = "white";
+  });
 
-  // ftpMobile
-  const ftpMobile = document.createElement("a");
-  ftpMobile.href =
-    "http://octptest.corp.ertelecom.loc/diagnostic-results/mobile/";
-  ftpMobile.target = "_blank";
-  ftpMobile.textContent = "FTP/ Мобила";
-  ftpMobile.classList.add("ml-2", "d-inline-block", "align-middle");
+  // ------------
 
-  // Настройка ТВ, стили
-  ftpMobile.style.fontSize = "1rem";
-  ftpMobile.style.color = "white";
-  ftpMobile.style.marginRight = "10px";
-  ftpMobile.style.paddingRight = "10px";
+  // Настройка приставок
+  const setupDecoder = document.createElement("a");
+  setupDecoder.href =
+    "https://perm.dom.ru/service/knowledgebase/domru-tv/nastrojka-tv-pristavok";
+  setupDecoder.target = "_blank";
+  setupDecoder.textContent = "Приставки";
+  setupDecoder.classList.add("ml-2", "d-inline-block", "align-middle");
+
+  // Настройка приставок, стили
+  setupDecoder.style.borderRight = "1px solid white";
+  setupDecoder.style.fontSize = "1rem";
+  setupDecoder.style.color = "white";
+  setupDecoder.style.marginRight = "10px";
+  setupDecoder.style.paddingRight = "10px";
+  setupDecoder.style.cursor = "pointer";
+  setupDecoder.addEventListener("mouseenter", () => {
+    setupDecoder.style.color = "#ccc";
+  });
+  setupDecoder.addEventListener("mouseleave", () => {
+    setupDecoder.style.color = "white";
+  });
+
+  // ------------
 
   // ftpPC
   const ftpPC = document.createElement("a");
   ftpPC.href = "http://octptest.corp.ertelecom.loc/diagnostic-results/perm/";
   ftpPC.target = "_blank";
-  ftpPC.textContent = "FTP/ ПК";
+  ftpPC.textContent = "FTP ПК";
   ftpPC.classList.add("ml-2", "d-inline-block", "align-middle");
 
   // Настройка ТВ, стили
@@ -81,14 +131,46 @@ function genesysButtons() {
   ftpPC.style.color = "white";
   ftpPC.style.marginRight = "10px";
   ftpPC.style.paddingRight = "10px";
+  ftpPC.style.cursor = "pointer";
+  ftpPC.addEventListener("mouseenter", () => {
+    ftpPC.style.color = "#ccc";
+  });
+  ftpPC.addEventListener("mouseleave", () => {
+    ftpPC.style.color = "white";
+  });
+
+  // ------------
+
+  // ftpMobile
+  const ftpMobile = document.createElement("a");
+  ftpMobile.href =
+    "http://octptest.corp.ertelecom.loc/diagnostic-results/mobile/";
+  ftpMobile.target = "_blank";
+  ftpMobile.textContent = "FTP Мобила";
+  ftpMobile.classList.add("ml-2", "d-inline-block", "align-middle");
+
+  // Настройка ТВ, стили
+  ftpMobile.style.fontSize = "1rem";
+  ftpMobile.style.color = "white";
+  ftpMobile.style.marginRight = "10px";
+  ftpMobile.style.paddingRight = "10px";
+  ftpMobile.style.cursor = "pointer";
+  ftpMobile.addEventListener("mouseenter", () => {
+    ftpMobile.style.color = "#ccc";
+  });
+  ftpMobile.addEventListener("mouseleave", () => {
+    ftpMobile.style.color = "white";
+  });
 
   const intervalId = setInterval(() => {
     const element = document.getElementById("break_window");
     if (element) {
       element.insertAdjacentElement("afterend", ftpMobile);
       element.insertAdjacentElement("afterend", ftpPC);
+      element.insertAdjacentElement("afterend", setupDecoder);
       element.insertAdjacentElement("afterend", setupTV);
       element.insertAdjacentElement("afterend", setupRouter);
+      element.insertAdjacentElement("afterend", chatMaster);
       clearInterval(intervalId);
     }
   }, 100);
