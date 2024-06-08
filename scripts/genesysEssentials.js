45a53d1863a9b6fa70eb7133e1165623f4d1ff0a
AuthFailed <authfailed@gmail.com>
2024-06-09 01:47:00 +0500

    Украшены кнопки в генезисе

--- scripts/genesysEssentials.js
+++ scripts/genesysEssentials.js
@@ -1,7 +1,20 @@
 if (document.URL.indexOf("genesys-app1") != -1) {
   genesysButtons();
   hideGenesysHelp();
-  hideHeader();
+
+  /* if (navigator.userAgent.includes("Chrome") == false) {
+    browser.storage.local.get(["hideHeader"]).then((result) => {
+      if (result.hideHeader == true) {
+        hideHeader();
+      }
+    });
+  } else {
+    chrome.storage.local.get(["hideHeader"], function (result) {
+      if (result.hideHeader == true) {
+        hideHeader();
+      }
+    });
+  } */
 }
 
 function hideHeader() {
@@ -9,8 +22,14 @@ function hideHeader() {
   // TODO добавить включение/отключение опции в настройках
   setInterval(() => {
     var chatHeader = document.querySelector(".wwe-case-information-header");
-    if (chatHeader.getAttribute("aria-expanded") == "true") {
-      chatHeader.click();
+    if (chatHeader) {
+      console.log("Хедер: найден");
+      if (chatHeader.getAttribute("aria-expanded") == "true") {
+        chatHeader.click();
+        console.log("Хедер: скрыт");
+      }
+    } else {
+      console.log("Хедер: не найден");
     }
   }, 1000);
 }
@@ -26,87 +45,108 @@ function hideGenesysHelp() {
 }
 
 function genesysButtons() {
-  const chatMaster = createGenesysLink(
-    "http://cm.roool.ru/",
-    "ЧМ",
-    { marginLeft: "20px" },
-    true
-  );
+  // Удаляем !important у border-radius для округления кнопок
+  var styleSheets = document.styleSheets;
+  for (var i = 0; i < styleSheets.length; i++) {
+    var rules = styleSheets[i].cssRules || styleSheets[i].rules;
+    if (!rules) continue;
+
+    for (var j = 0; j < rules.length; j++) {
+      if (
+        rules[j].selectorText ===
+        ".wwe input, .wwe select, .wwe button, .wwe textarea"
+      ) {
+        // Убираем !important из стиля
+        rules[j].style.borderRadius = "0px";
+        rules[j].style.setProperty("border-radius", "0px", "");
+      }
+    }
+  }
+
+  let buttonsDiv = document.createElement("div");
+  buttonsDiv.style.display = "flex";
+  buttonsDiv.style.justifyContent = "center";
+  buttonsDiv.style.alignItems = "center";
+  buttonsDiv.style.height = "100%";
+  buttonsDiv.style.marginLeft = "15px";
+
+  const chatMaster = createGenesysLink("http://cm.roool.ru/", "ЧМ");
 
   const setupRouter = createGenesysLink(
-    "https://interzet.dom.ru/service/knowledgebase/internet/kak-nastroit-router",
+    "https://dom.ru/service/knowledgebase/internet/kak-nastroit-router",
     "Роутеры"
   );
 
   const setupTV = createGenesysLink(
-    "https://oren.dom.ru/faq/televidenie/kak-nastroit-cifrovye-kanaly-na-televizore",
-    "Телевизоры"
+    "https://dom.ru/faq/televidenie/kak-nastroit-cifrovye-kanaly-na-televizore",
+    "ТВ"
   );
 
   const setupDecoder = createGenesysLink(
-    "https://perm.dom.ru/service/knowledgebase/domru-tv/nastrojka-tv-pristavok",
-    "Приставки",
-    {},
-    true
+    "https://dom.ru/service/knowledgebase/domru-tv/nastrojka-tv-pristavok",
+    "Декодеры"
   );
 
   const ftpPC = createGenesysLink(
-    "http://octptest.corp.ertelecom.loc/diagnostic-results/perm/",
+    "http://octptest.corp.ertelecom.loc/diagnostic-results/perm/?C=M;O=D",
     "FTP ПК"
   );
 
   const ftpMobile = createGenesysLink(
-    "http://octptest.corp.ertelecom.loc/diagnostic-results/mobile/",
-    "FTP Мобила"
+    "http://octptest.corp.ertelecom.loc/diagnostic-results/mobile/?C=M;O=D",
+    "FTP Моб"
   );
 
   const intervalId = setInterval(() => {
-    const element = document.getElementById("break_window");
-    if (element) {
-      element.insertAdjacentElement("afterend", ftpMobile);
-      element.insertAdjacentElement("afterend", ftpPC);
-      element.insertAdjacentElement("afterend", setupDecoder);
-      element.insertAdjacentElement("afterend", setupTV);
-      element.insertAdjacentElement("afterend", setupRouter);
-      element.insertAdjacentElement("afterend", chatMaster);
+    let lineHeader = document.getElementById("break_window");
+
+    lineHeader.parentNode.insertBefore(buttonsDiv, lineHeader.nextSibling);
+    if (lineHeader) {
+      buttonsDiv.appendChild(chatMaster);
+      buttonsDiv.appendChild(setupRouter);
+      buttonsDiv.appendChild(setupDecoder);
+      buttonsDiv.appendChild(setupTV);
+      buttonsDiv.appendChild(ftpPC);
+      buttonsDiv.appendChild(ftpMobile);
       clearInterval(intervalId);
     }
-  }, 100);
+  }, 3000);
 }
 
-function createGenesysLink(
-  href,
-  textContent,
-  additionalStyles = {},
-  borderRight = false
-) {
-  const link = document.createElement("a");
-  link.href = href;
-  link.target = "_blank";
-  link.textContent = textContent;
-  link.classList.add("ml-2", "d-inline-block", "align-middle");
+function createGenesysLink(href, textContent, additionalStyles = {}) {
+  const button = document.createElement("button");
+  button.href = href;
+  button.target = "_blank";
+  button.textContent = textContent;
 
-  // Apply common styles
-  Object.assign(link.style, {
+  // Применение общих стилей
+  Object.assign(button.style, {
     fontSize: "1rem",
+    fontFamily: "Roboto, Tahoma, Verdana",
+    textAlign: "center",
     color: "white",
-    lineHeight: "40px",
-    marginRight: "10px",
-    paddingRight: "10px",
+    marginRight: "8px",
     cursor: "pointer",
+    height: "28px",
+    width: "100px",
+    lineHeight: "auto",
+    backgroundColor: "#4c5961",
+    border: "none",
+    borderRadius: "18px",
+    lineHeight: "20px",
+    borderRadius: "18px",
     additionalStyles,
   });
 
-  if (borderRight) {
-    link.style.borderRight = "1px solid white";
-  }
-
-  link.addEventListener("mouseenter", () => {
-    link.style.color = "#ccc";
+  button.addEventListener("mouseenter", () => {
+    button.style.backgroundColor = "#63737d";
   });
-  link.addEventListener("mouseleave", () => {
-    link.style.color = "white";
+  button.addEventListener("mouseleave", () => {
+    button.style.backgroundColor = "#4c5961";
+  });
+  button.addEventListener("click", function () {
+    window.open(href, "_blank");
   });
 
-  return link;
+  return button;
 }
