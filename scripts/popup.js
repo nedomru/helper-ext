78bb4ed8b3547b820a8413dd537ed803fad0ca8f
AuthFailed <authfailed@gmail.com>
2024-06-11 01:40:26 +0500

    Убрана проверка на regex у сокращателя ссылок

--- scripts/popup.js
+++ scripts/popup.js
@@ -114,20 +114,6 @@ async function handleFormSubmitLink(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
   const inputField = formData.get("input-link");
-  const url_regex = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name and extension
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$",
-    "i"
-  );
-
-  // Проверка корректности ввода ссылки
-  if (url_regex.test(inputField) == false) {
-    $.notify("Это не ссылка", "error");
-    return;
-  }
 
   $.notify("Сокращаю", "info");
   try {
