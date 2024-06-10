723112edb5940b66f402f885ba4447a60ad5c133
AuthFailed <authfailed@gmail.com>
2024-06-11 02:12:30 +0500

    Добавлено выделение цветом активности гарантии на оборудование

--- scripts/highlights.js
+++ scripts/highlights.js
@@ -11,14 +11,47 @@ const colors = {
 };
 
 // Функция для подсветки текста
-function highlightText(element, searchText, color, text) {
-  if (
-    element.innerText.includes(searchText) &&
-    !element.querySelector("span")
-  ) {
-    element.innerHTML = element.innerHTML.replace(
-      searchText,
-      `<span style='color: ${color}; font-weight: bold'>${text}</span>`
+function highlightText(element) {
+  let text = element.innerText;
+
+  // Проверка наличия текста "гарантийный срок до" и даты
+  let dateRegex = /\d{2}\.\d{2}\.\d{4}/; // Регулярное выражение для поиска даты в формате dd.mm.yyyy
+  let match = text.match(dateRegex);
+  if (text.includes("гарантийный срок до") && match) {
+    let date = new Date(match[0].split(".").reverse().join("-")); // Преобразование даты в формат 'yyyy-mm-dd'
+    let currentDate = new Date();
+    if (date > currentDate) {
+      element.innerHTML = text.replace(
+        match[0],
+        `<span style='color: green; font-weight: bold'>${match[0]}</span>`
+      );
+    } else {
+      element.innerHTML = text.replace(
+        match[0],
+        `<span style='color: red; font-weight: bold'>${match[0]}</span>`
+      );
+    }
+  }
+
+  // Проверка других условий выделения текста
+  if (text.includes(textToFind_cs)) {
+    element.innerHTML = text.replace(
+      textToFind_cs,
+      `<span style='color: ${colors.cs}; font-weight: bold'>${textToFind_cs}</span>`
+    );
+  }
+
+  if (text.includes(textToFind_email)) {
+    element.innerHTML = text.replace(
+      textToFind_email,
+      `<span style='color: ${colors.email}; font-weight: bold'>${textToFind_email}</span>`
+    );
+  }
+
+  if (text.includes(textToFind_octp)) {
+    element.innerHTML = text.replace(
+      textToFind_octp,
+      `<span style='color: ${colors.octp}; font-weight: bold'>${textToFind_octp}</span>`
     );
   }
 }
@@ -42,9 +75,7 @@ setInterval(function () {
   ) {
     const cells = document.querySelectorAll("td");
     cells.forEach((cell) => {
-      highlightText(cell, textToFind_cs, colors.cs, textToFind_cs);
-      highlightText(cell, textToFind_email, colors.email, textToFind_email);
-      highlightText(cell, textToFind_octp, colors.octp, textToFind_octp);
+      highlightText(cell);
     });
   }
 }, 1000);
