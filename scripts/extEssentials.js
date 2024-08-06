d7c5a9e8634a3ac89c11391277f403ff01ced9da
AuthFailed <authfailed@gmail.com>
2024-08-06 14:03:04 +0500

    Фикс сравнения версий

--- scripts/extEssentials.js
+++ scripts/extEssentials.js
@@ -1,4 +1,16 @@
 async function checkForUpdates() {
+  function compareVersions(v1, v2) {
+    const v1Parts = v1.split(".").map(Number);
+    const v2Parts = v2.split(".").map(Number);
+    for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
+      const v1Part = v1Parts[i] || 0;
+      const v2Part = v2Parts[i] || 0;
+      if (v1Part < v2Part) return -1; // v1 меньше v2
+      if (v1Part > v2Part) return 1; // v1 больше v2
+    }
+    return 0; // версии равны
+  }
+
   const response = await fetch(
     "https://api.github.com/repos/AuthFailed/domhelper/releases/latest"
   );
@@ -6,8 +18,9 @@ async function checkForUpdates() {
   const data = await response.json();
 
   const latestVersion = data.tag_name;
+  const currentVersion = browser.runtime.getManifest().version;
 
-  if (latestVersion !== browser.runtime.getManifest().version) {
+  if (compareVersions(latestVersion, currentVersion) > 0) {
     browser.tabs.create({
       url: browser.runtime.getURL("update.html"),
     });
