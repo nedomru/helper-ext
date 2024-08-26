b09c304566f4fc4f2f550e6f23c62e7d119ad0cc
Roman Chursanov <authfailed@gmail.com>
2024-08-26 16:29:50 +0500

    [Доработка] Оптимизировать подсветку Fixes #88

--- scripts/highlights.js
+++ scripts/highlights.js
@@ -1,3 +1,26 @@
+// Подсветка классификаторов на вкладке Обращений
+if (document.URL.indexOf("db.ertelecom.ru/cgi-bin") != -1) {
+  getHighlightColors(() => {
+    const intervalId = setInterval(highlightRequestsTab, 1000); // если необходимо продолжать
+  });
+}
+
+// Подсветка классификаторов на вкладке Изменения обращений
+if (
+  document.URL.indexOf("wcc_request_appl_support.change_request_appl") != -1
+) {
+  highlightRequestsEdit();
+}
+
+// Подсветка компенсации за аварию в операциях с договором
+if (
+  document.URL.indexOf(
+    "db.ertelecom.ru/cgi-bin/ppo/excells/adv_act_retention"
+  ) != -1
+) {
+  highlightCompensation();
+}
+
 let dataToHighlight = {};
 
 // Функция для получения значения цвета из color picker
@@ -50,57 +73,42 @@ function highlightText(element) {
   }
 }
 
-function highlightCells() {
-  if (document.URL.includes("db.ertelecom.ru/cgi-bin")) {
-    const rows = document.querySelectorAll("tr");
-    rows.forEach((row) => {
-      const cells = row.querySelectorAll("td");
+function highlightRequestsTab() {
+  const rows = document.querySelectorAll("tr");
+  rows.forEach((row) => {
+    const cells = row.querySelectorAll("td");
 
-      // Проверяем, что в ячейке с индексом 10 (11-й элемент) находится текст ссылки
-      if (cells.length > 10) {
-        const linkCell = cells[10]; // берём 11-ю ячейку
-        highlightText(linkCell);
+    // Проверяем, что в ячейке с индексом 10 (11-й элемент) находится текст ссылки
+    if (cells.length > 10) {
+      const linkCell = cells[10]; // берём 11-ю ячейку
+      if (linkCell.classList.contains("helper-highlighted")) {
+        clearInterval(intervalId);
       }
-    });
-  }
-}
-
-let intervalId = setInterval(highlightCells, 1000);
-
-// Дополнительная функция для обработки других страниц
-function handleOtherPages() {
-  if (document.URL.includes("wcc_request_appl_support.change_request_appl")) {
-    const block = document.querySelector(".col-sm-9");
-    if (block) {
-      block.innerHTML = block.innerHTML.replaceAll(
-        "Контакт сорвался",
-        "<font style='color: red; font-weight:bold'>Контакт сорвался</font>"
-      );
+      highlightText(linkCell);
+      linkCell.classList += "helper-highlighted";
     }
-  } else if (
-    document.URL.includes(
-      "db.ertelecom.ru/cgi-bin/ppo/excells/adv_act_retention"
-    )
-  ) {
-    const cells = document.querySelectorAll("th");
-    cells.forEach((cell) => {
-      if (cell.innerText === "Компенсация за аварию") {
-        cell.style.color = "black";
-        cell.style.backgroundColor = "white";
-        const tdCell = cell.parentElement.querySelector("td");
-        tdCell.style.color = "black";
-        tdCell.style.backgroundColor = "white";
-      }
-    });
+  });
+}
+
+function highlightRequestsEdit() {
+  const block = document.querySelector(".col-sm-9");
+  if (block) {
+    block.innerHTML = block.innerHTML.replaceAll(
+      "Контакт сорвался",
+      "<font style='color: red; font-weight:bold'>Контакт сорвался</font>"
+    );
   }
 }
 
-// Вызов функции и передача коллбэка
-getHighlightColors(() => {
-  // Запуск обработки после получения цветов
-  highlightCells(); // это можно вызывать один раз вместо intervalId
-  setInterval(highlightCells, 1000); // если необходимо продолжать
-});
-
-// Вызываем обработчик для других страниц сразу
-handleOtherPages();
+function highlightCompensation() {
+  const cells = document.querySelectorAll("th");
+  cells.forEach((cell) => {
+    if (cell.innerText === "Компенсация за аварию") {
+      cell.style.color = "black";
+      cell.style.backgroundColor = "white";
+      const tdCell = cell.parentElement.querySelector("td");
+      tdCell.style.color = "black";
+      tdCell.style.backgroundColor = "white";
+    }
+  });
+}
