a9be0c52b8d7b81d836aede2f863c279ad43061d
Roman Chursanov <authfailed@gmail.com>
2024-09-19 11:00:29 +0500

    Добавлена ассинхронность и проверка сессии

--- scripts/darkTheme.js
+++ scripts/darkTheme.js
@@ -1,6 +1,10 @@
-function skvozRequests() {
+async function applyStyles(styleContent) {
   const style = document.createElement("style");
+  style.appendChild(document.createTextNode(styleContent));
+  document.head.appendChild(style);
+}
 
+async function skvozRequests() {
   const darkThemeCSS = `
 body {
   background-color: #1c1c1c !important;
@@ -13,30 +17,29 @@ td {
   color: #ffffff !important;
 }
 select {
-  background-color: #363535 !important; /* Dark background */
-  color: #ffffff !important; /* White text */
-  border: 1px solid #444444 !important; /* Dark border */
-  appearance: none; /* Remove default styles */
-  -moz-appearance: none; /* For Firefox */
+  background-color: #363535 !important;
+  color: #ffffff !important;
+  border: 1px solid #444444 !important;
+  appearance: none;
+  -moz-appearance: none;
 }
 select option {
   background-color: #1e1e1e !important;
   color: #ffffff !important;
 }
 select:focus {
-  outline: none; /* Remove outline */
+  outline: none;
   border-color: #0060df !important;
 }
 textarea {
   background-color: #1e1e1e !important;
   color: #ffffff !important;
-  border: 1px solid #444444 !important; /* Dark border */
-  appearance: none; /* Remove default styles */
+  border: 1px solid #444444 !important;
 }
 input {
   background-color: #1e1e1e !important;
   color: #ffffff !important;
-  border: 1px solid #444444 !important; /* Dark border */
+  border: 1px solid #444444 !important;
 }
 #ui-datepicker-div {
   color: #ffffff !important;
@@ -57,40 +60,33 @@ input {
 .ui-accordion .ui-accordion-header {
   background: #2a2a2a !important;
   border-bottom: 1px solid #444444 !important;
-  color: #ffffff !important; /* Header text */
+  color: #ffffff !important;
 }
 .ui-accordion .ui-accordion-content {
   background: #1e1e1e !important;
-  color: #ffffff !important; /* Accordion content text */
+  color: #ffffff !important;
 }
 .btn, button {
   background-color: #3c3c3c !important;
-  color: #ffffff !important; /* Button text */
+  color: #ffffff !important;
 }
 .notifyjs-bootstrap-base {
-  background-color: #333 !important; /* Notify background */
-  color: #ffffff !important; /* Notify text */
+  background-color: #333 !important;
+  color: #ffffff !important;
 }
 .notifyjs-bootstrap-error {
-  background-color: #7c4d4d !important; /* Error notify */
+  background-color: #7c4d4d !important;
   color: #ffffff !important;
 }
 .notifyjs-bootstrap-success {
-  background-color: #4caf50 !important; /* Success notify */
+  background-color: #4caf50 !important;
   color: #ffffff !important;
 }
     `;
-
-  // Append the CSS to the style element
-  style.appendChild(document.createTextNode(darkThemeCSS));
-
-  // Append the style element to the head of the document
-  document.head.appendChild(style);
+  await applyStyles(darkThemeCSS);
 }
 
-function clientFind() {
-  const style = document.createElement("style");
-
+async function clientFind() {
   const darkThemeCSS = `
 body {
   background-color: #1c1c1c !important;
@@ -103,30 +99,29 @@ td {
   color: #ffffff !important;
 }
 select {
-  background-color: #363535 !important; /* Dark background */
-  color: #ffffff !important; /* White text */
-  border: 1px solid #444444 !important; /* Dark border */
-  appearance: none; /* Remove default styles */
-  -moz-appearance: none; /* For Firefox */
+  background-color: #363535 !important;
+  color: #ffffff !important;
+  border: 1px solid #444444 !important;
+  appearance: none;
+  -moz-appearance: none;
 }
 select option {
   background-color: #1e1e1e !important;
   color: #ffffff !important;
 }
 select:focus {
-  outline: none; /* Remove outline */
+  outline: none;
   border-color: #0060df !important;
 }
 textarea {
   background-color: #1e1e1e !important;
   color: #ffffff !important;
-  border: 1px solid #444444 !important; /* Dark border */
-  appearance: none; /* Remove default styles */
+  border: 1px solid #444444 !important;
 }
 input {
   background-color: #1e1e1e !important;
   color: #ffffff !important;
-  border: 1px solid #444444 !important; /* Dark border */
+  border: 1px solid #444444 !important;
 }
 .ui-state-default,
 .ui-widget-content .ui-state-default {
@@ -143,85 +138,96 @@ input {
 .ui-accordion .ui-accordion-header {
   background: #2a2a2a !important;
   border-bottom: 1px solid #444444 !important;
-  color: #ffffff !important; /* Header text */
+  color: #ffffff !important;
 }
 .ui-accordion .ui-accordion-content {
   background: #1e1e1e !important;
-  color: #ffffff !important; /* Accordion content text */
+  color: #ffffff !important;
 }
 .btn, button {
   background-color: #3c3c3c !important;
-  color: #ffffff !important; /* Button text */
+  color: #ffffff !important;
 }
     `;
-
-  // Append the CSS to the style element
-  style.appendChild(document.createTextNode(darkThemeCSS));
-
-  // Append the style element to the head of the document
-  document.head.appendChild(style);
+  await applyStyles(darkThemeCSS);
 }
 
-function topFrame() {
-  const style = document.createElement("style");
-
+async function topFrame() {
   const darkThemeCSS = `
 body {
   background-color: #1c1c1c !important;
   color: #ffffff !important;
 }
 #btnViewKnowlegeBase {
-background-color: #03545e !important
+  background-color: #03545e !important;
 }
-
 #actual_number {
-background-color: #4a2021 !important
+  background-color: #4a2021 !important;
 }
-
 #btnViewScripting {
-background-color: #665106 !important
+  background-color: #665106 !important;
 }
-
 #number_cc_field {
   background-color: #1c1c1c !important;
 }
-
 a, b {
   color: #ffffff !important;
 }
-
 .ui-accordion-header > form:nth-child(3) > div:nth-child(4) {
   background-color: #1c1c1c !important;
 }
     `;
-
-  // Append the CSS to the style element
-  style.appendChild(document.createTextNode(darkThemeCSS));
-
-  // Append the style element to the head of the document
-  document.head.appendChild(style);
+  await applyStyles(darkThemeCSS);
 }
 
-if (
-  document.URL.indexOf(
-    "ertelecom.ru/cgi-bin/ppo/excells/wcc2_main.frame_left_reasons"
-  ) != -1
-) {
-  skvozRequests();
+async function findSession() {
+  const darkThemeCSS = `
+body {
+  background-color: #1c1c1c !important;
+  color: #ffffff !important;
+}
+a, b {
+  color: #ffffff !important;
+}
+span.mac {
+  color: #0099ff
+}
+    `;
+  await applyStyles(darkThemeCSS);
 }
 
-if (
-  document.URL.indexOf(
-    "ertelecom.ru/cgi-bin/ppo/excells/wcc2_main.frame_left"
-  ) != -1
-) {
-  clientFind();
+async function applyTheme() {
+  if (
+    document.URL.includes(
+      "ertelecom.ru/cgi-bin/ppo/excells/wcc2_main.frame_left_reasons"
+    )
+  ) {
+    await skvozRequests();
+  }
+
+  if (
+    document.URL.includes(
+      "ertelecom.ru/cgi-bin/ppo/excells/wcc2_main.frame_left"
+    )
+  ) {
+    await clientFind();
+  }
+
+  if (
+    document.URL.includes(
+      "ertelecom.ru/cgi-bin/ppo/excells/wcc2_main.frame_top"
+    )
+  ) {
+    await topFrame();
+  }
+
+  if (
+    document.URL.includes(
+      "ertelecom.ru/cgi-bin/ppo/excells/radius_accounting_info.login_detail"
+    )
+  ) {
+    await findSession();
+  }
 }
 
-if (
-  document.URL.indexOf(
-    "ertelecom.ru/cgi-bin/ppo/excells/wcc2_main.frame_top"
-  ) != -1
-) {
-  topFrame();
-}
+applyTheme();
