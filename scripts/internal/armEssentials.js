984e62f3652b9eb42d9044fd3be94f09d9405f9d
Roman Chursanov <authfailed@gmail.com>
2024-12-01 00:16:23 +0500

    Перенесен функция копирования текста в файл функций ARM'a

--- scripts/internal/armEssentials.js
+++ scripts/internal/armEssentials.js
@@ -1,4 +1,19 @@
 if (
+    document.URL.indexOf("ertelecom.ru/cgi-bin/ppo/excells") !== -1) {
+  const config = {
+    ARM_allowCopy: allowCopy
+  };
+
+  browser.storage.sync.get(Object.keys(config)).then((result) => {
+    Object.keys(config).forEach((key) => {
+      if (result[key]) {
+        config[key]();
+      }
+    });
+  });
+}
+
+if (
   document.URL.indexOf(
     "db.ertelecom.ru/static_pages/private/wcc/client_session/?user_id",
   ) !== -1
@@ -2693,3 +2708,20 @@ function addToggleInfoButton(container) {
   container.insertBefore(buttonContainer, container.firstChild);
 }
 
+async function allowCopy() {
+  setTimeout(async () => {
+    const waiter = document.getElementById("waiter");
+    if (!waiter) return;
+
+    const {parentNode: parent} = waiter;
+    parent.style = "";
+    parent.onselectstart = "";
+
+    const formform = document.getElementById("formform");
+    if (!formform) return;
+
+    [waiter, formform].forEach((el) => {
+      el.onmousedown = el.onselectstart = (event) => event.stopPropagation();
+    });
+  }, 2000);
+}
